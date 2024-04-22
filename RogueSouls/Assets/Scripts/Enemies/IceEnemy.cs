83d99803f36340ebcf6551464a3914c8b5ff5b9c
busch-owen <buschowen@gmail.com>
2024-04-22 14:16:40 -0600

    Bug fixes and playtesting

--- RogueSouls/Assets/Scripts/Enemies/IceEnemy.cs
+++ RogueSouls/Assets/Scripts/Enemies/IceEnemy.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class IceEnemy : Enemy
 {
-    [SerializeField]
-    bool iceArmor;
-    [SerializeField]
-    int armorDamper = 50;
 
-
-    public override void TakeDamage(int damage)
-    {
-        if (iceArmor)
-        {
-            damage *= (armorDamper / 100);
-        }
-        base.TakeDamage(damage);
-        //if( _health > 50%) // If health Falls below 50 the icearmor gets removed and takes straight damage
-        {
-        }
-    }
 }
